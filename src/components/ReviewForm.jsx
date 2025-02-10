5367dd3d73ea8281db16c5fc323361be8bf724eb
luciameneghin <lucia.meneghin00@gmail.com>
2025-02-10 15:39:49 +0100

    feat: added review form

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const ReviewForm = ({ movie_id, fetchData }) => {
+  const api_url = `${import.meta.env.VITE_API_URL}/api/movies/${movie_id}/reviews`;
+  const redirect = useNavigate();
+
+  const initialFormData = {
+    text: '',
+    name: '',
+    vote: '',
+    movie_id: 0
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const validateForm = () => {
+    // console.log(typeof formData.vote);
+    if (!formData.name || !formData.text) return false
+    if (isNaN(formData.vote) || formData.vote < 1 || formData.vote > 5) return false
+
+    return true
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log('invio');
+    //validazione
+    if (!validateForm()) {
+      setErrorMessage('Attenzione, nome e testo sono obbligatori e il voto deve essere un numero compreso tra 1 e 5')
+      return
+    }
+    //......
+    //console.log(formData); //devo vedere quello che inserisco nei campi di input 
+    // console.log(api_url);
+    axios.post(api_url, formData, { headers: { 'Content-Type': 'application/json' } })
+      .then(res => {
+        console.log(res.data);
+        setFormData(initialFormData); //svuoto form
+        //cancello eventuale messaggio di errore
+        setErrorMessage('')
+        //ricarico recensioni
+        fetchData(movie_id, () => redirect('/404'))
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+
+  const setFieldValue = (e) => {
+    const { value, name } = e.target;
+    // console.log(value, name);
+    setFormData((prev => ({ ...prev, [name]: value })));
+  }
+
+
+  return (
+    <div className="card my-3">
+      <header className="card-header">
+        <h1>Aggiungi una recensione</h1>
+      </header>
+      <div className="card-body">
+        <p className="text-danger">{errorMessage}</p>
+        <form action="#" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name='name'
+              placeholder="inserisci nome"
+              value={formData.name}
+              onChange={setFieldValue}
+            />
+          </div>
+          <div className="form-group">
+            <label>Testo</label>
+            <textarea
+              name="text"
+              type="text"
+              className="form-control"
+              placeholder="inserisci testo"
+              value={formData.text}
+              onChange={setFieldValue}
+            >
+            </textarea>
+          </div>
+          <div className="form-group">
+            <label>Voto</label>
+            <input
+              type="number"
+              className="form-control"
+              name='vote'
+              min={1}
+              max={5}
+              placeholder="inserisci voto"
+              value={formData.vote}
+              onChange={setFieldValue}
+            />
+          </div>
+          <div className="d-flex justify-content-end ">
+            <button className="btn btn-primary" type="submit">Invia</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewForm
--- src/pages/FilmDetailsPage.jsx
+++ src/pages/FilmDetailsPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useGlobalContext } from "../context/GlobalContext";
 import ReviewsCard from "../components/ReviewsCard";
+import Stars from "../components/Stars";
 import { Link } from "react-router-dom";
 import ReviewForm from "../components/ReviewForm";
 
@@ -13,37 +14,35 @@ const FilmDetailsPage = () => {
     fetchFilm(id);
   }, [id]);
 
+
   return (
     <div className="container mt-5 border-bottom border-1">
       <header>
         <h2>Dettagli del Film</h2>
         <div className="d-flex">
-          <img className="w-50" src={film?.image} alt={film?.title} />
+          <img className="w-25" src={film?.image} alt={film?.title} />
           <div className="p-4">
             <h1>{film?.title}</h1>
             <h3>{film?.director}</h3>
             <p>{film?.abstract}</p>
+            <div><Stars /></div>
           </div>
         </div>
       </header>
 
       <section className="film-reviews">
         <h3>Recensioni</h3>
-        {/* {film.reviews.map(review => (
-          <div key={review.id} className="review-container">
-            <p>{review.name}</p>
-            <p>{review.text}</p>
-          </div>
-        ))} */}
         {film?.reviews && (
           <ReviewsCard data={film?.reviews} />
-
         )}
       </section>
 
       <section>
-        {/* <ReviewForm/> */}
+        {film && (
+          <ReviewForm movie_id={id} fetchData={fetchFilm} />
+        )}
       </section>
+
       <footer>
         <Link to='/' className='btn btn-primary'>Ritorna all'elenco</Link>
       </footer>
